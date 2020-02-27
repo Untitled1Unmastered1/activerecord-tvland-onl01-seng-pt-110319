a82e4b49ad89ff9a4cc976a7ea5f074a6a230330
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-27 00:46:18 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     self.characters.map do |x|
-      "#{x.characters} - #{x.shows}
+      "#{x.characters} - #{x.shows}"
     end 
   end 
 end 
